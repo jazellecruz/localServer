37c70443e9e60a29a822f2cdb5e4f0774d4bc2dc
jazzcruz <jazellecruz22@gmail.com>
2024-02-20 22:58:02 +0800

    add logic handling for args

--- bin/bin.js
+++ bin/bin.js
@@ -1,9 +1,33 @@
 #!/usr/bin/env node
+let initialize = require("../lib/server");
 
 let args = process.argv.slice(2);
 
-args.forEach(arg => {
+let defaultOptions = { port: 8000, dir: "." };
+let userOptions = {};
 
+args.forEach(arg => {
+  let opt = arg.split("=");
+
+  if(opt[0] === "--port") {
+    userOptions.port = opt[1];
+  } else  if(opt[0] === "--dir") {
+    console.log(opt[1][0])
+    if (opt[1][0] === "/") {
+      opt[1] = opt[0].replace(opt[1][0], "");
+    }
+
+    userOptions.dir = opt[1];
+  }  else {
+    console.log("wrong option (?_?)");
+    return;
+  }
 });
 
-console.log(args)
+
+userOptions = Object.assign(defaultOptions, userOptions);
+
+initialize(userOptions);
+
+
+
